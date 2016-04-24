b187cc36b547d9bff39daee653825086571f9a69
Caleb Sumpter <calebsump@gmail.com>
2016-04-23 22:30:57 -0400

    Sorcery authentication complete

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	before_filter :require_login, only: [:new, :create, :update, :destroy, :edit]
 	include ArticlesHelper
 	def index
 		@articles = Article.all
